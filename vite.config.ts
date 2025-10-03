4ee6cf773e10cc10380e1182d30599a06a515b80
Noah <noah@MacBookPro.home>
2025-10-03 09:14:51 +0200

    remove gh pages conifg

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: "/digitale-kiosk-sunshower",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
